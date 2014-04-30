d02f6ba5aab98c401dd93cf27f99bf1223e69a99
monkey413 <monkey413@gmail.com>
2014-04-30 21:11:49 +0800

    finish command line options

--- createFileDir.lisp
+++ createFileDir.lisp
@@ -8,15 +8,14 @@
        ,@body)))
 
 (ql:quickload "cl-utilities")
-
-(format t "~a~%" (nth 1 *posix-argv*))
+(ql:quickload "getopt")
+  
 
 (defun createDirOrFile (path)
   (if (equal (file-namestring path) "")
       (ensure-directories-exist path)
-      (with-open-file (stream path :direction :output))
-      )
-  )
+      (with-open-file (stream path :direction :output :if-exists nil))
+      ))
 
 (defun getFileLines (file)
   (let ((in (open file)) (lines nil))
@@ -28,19 +27,21 @@
     lines))
 
 (defun main ()
-  (let* ((file (truename (nth 1 *posix-argv*)))
-         (fileLines (getFileLines file))
-         (maxDepth (apply #'max (mapcar #'length fileLines)))
-         (prefix ""))
+  (let* ((file (probe-file (cdr (assoc "i" (multiple-value-bind (args options)
+                                             (getopt:getopt *posix-argv* '(("i" :required))) options) :test #'equal))))
+           (fileLines (getFileLines file))
+           (maxDepth (apply #'max (mapcar #'length fileLines)))
+           (prefix ""))
     (for j 0 (- maxDepth 2)
-         (setf prefix "")
-         (dolist (i fileLines)
-           (block continue
-             (if (null (nth j i)) (return-from continue))
-             (if (equal (nth j i) "") (setf (nth j i) prefix))
-             (if (and (nth j i) (not (equal (nth j i) ""))) (setf prefix (nth j i)) )
-            ))
-         )
+      (setf prefix "")
+      (dolist (i fileLines)
+        (block continue
+          (if (null (nth j i)) (return-from continue))
+          (if (equal (nth j i) "") (setf (nth j i) prefix))
+          (if (and (nth j i) (not (equal (nth j i) ""))) (setf prefix (nth j i)) )
+          ))
+      )
     (setf fileLines  (mapcar #'(lambda (x) (apply #'concatenate 'string "./" x)) fileLines))
-    (dolist (path fileLines) (createDirOrFile path))
-    ))
+    (dolist (path fileLines) (createDirOrFile path))))
+
+(main)
